e177950e9fbb6a57fb60a319e31545dfd8bd606e
Aron <aronwc@gmail.com>
2024-05-06 13:01:22 -0500

    debug

--- ant/settings.py
+++ ant/settings.py
@@ -43,8 +43,10 @@ SECRET_KEY = os.environ.get(
 IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU_APP:
-    DEBUG = True
+DEBUG = True
+# turn me off
+# if not IS_HEROKU_APP:
+    # DEBUG = True
 
 # On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS``, since the Heroku router performs
 # validation of the Host header in the incoming HTTP request. On other platforms you may need
